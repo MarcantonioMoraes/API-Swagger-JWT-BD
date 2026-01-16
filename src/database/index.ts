660bc2da0bc4c6a5557a499ea685fa64e386cf8e
Marcantonio <marcantonio.lupercino@gmail.com>
2026-01-16 19:42:49 -0300

    .

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,23 +1,4 @@
-import express from "express"
 import { PrismaClient } from "@prisma/client"
-import { env } from "process"
 
 export const prisma = new PrismaClient()
 
-const app = express()
-const port = 3000
-
-// Middleware para ler JSON
-app.use(express.json())
-
-app.get("/", (req, res) => {
-  res.send("API rodando com Express + TypeScript 🚀")
-})
-
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`)
-})
-
-datasource: {
-  url: env("DATABASE_URL"),
-}
